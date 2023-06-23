4815dafad9e7a051b1adbed9e20ac793e5fff048
Constantine7s <k.shambazov@gmail.com>
2023-06-23 12:33:46 +0900

    feat: update single movie page to fetch corresponding film data

--- src/pages/SingleMoive.js
+++ src/pages/SingleMoive.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+
 
 function SingleMoive() {
+  const { id } = useParams()
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(`https://ghibliapi.vercel.app/films/${id}`
+        );
+        console.log(result);
+        setMovie(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [])
+
   return (
-    <div>SingleMoive</div>
+    <div>{movie.title}</div>
   )
 }
 
