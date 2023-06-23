9995ae8567dc839b8b1f6abdee11479be2d2eb68
Constantine7s <k.shambazov@gmail.com>
2023-06-23 12:32:10 +0900

    feat: update movie tile to link to corresponding film

--- src/components/MovieTile.js
+++ src/components/MovieTile.js
@@ -1,27 +1,31 @@
-import { Card, CardBody, Image, Heading, Text, Flex } from '@chakra-ui/react';
+import { Box, LinkBox, Image, Heading, Text, Flex, LinkOverlay } from '@chakra-ui/react';
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function MovieTile({ movie }) {
   return (
-    <Card maxW='sm' variant='outline' marginY='3' marginX='1' backgroundColor="#FDF2E9"
+    <LinkBox maxW='sm' padding='5' borderRadius='5' marginY='3' marginX='1' backgroundColor="#FDF2E9"
       _hover={{ backgroundColor: '#F6DDCC' }}>
-      <CardBody>
-        <Image
-          src={movie.image}
-          alt={movie.title}
-          borderRadius='4g'
-        />
-        <Heading size='md' marginTop='3'>{movie.title}</Heading>
-        <Flex mt='6' spacing='3' justifyContent='space-between'>
-          <Text>
-            {movie.release_date}
-          </Text>
-          <Text>
-            {movie.running_time} minutes
-          </Text>
-        </Flex>
-      </CardBody>
-    </Card>
+      <LinkOverlay href={`/movie/${movie.id}`} >
+        <Box>
+          <Image
+            src={movie.image}
+            alt={movie.title}
+            borderRadius='1'
+          />
+          <Heading size='md' marginTop='3'>{movie.title}</Heading>
+          <Flex mt='6' spacing='3' justifyContent='space-between'>
+            <Text>
+              {movie.release_date}
+            </Text>
+            <Text>
+              {movie.running_time} minutes
+            </Text>
+          </Flex>
+        </Box>
+
+      </LinkOverlay>
+    </LinkBox>
   );
 }
 
