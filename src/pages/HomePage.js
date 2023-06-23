00ca20765f1f2f05f5ba8dee22f86c52d6b30737
Constantine7s <k.shambazov@gmail.com>
2023-06-23 21:16:41 +0900

    feat: add search bar and filter by date or title functionality

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import MovieTile from '../components/MovieTile';
-import { Flex } from '@chakra-ui/react';
+import { Box, Input } from '@chakra-ui/react';
 
 function HomePage() {
 
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('')
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const result = await axios.get('https://ghibliapi.vercel.app/films');
-          setData(result.data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
   
-      fetchData();
-    }, []);
-  
-    return (
-        <Flex flexWrap='wrap' justifyContent={'space-evenly'}>
-          {data.map(movie => (
-            <MovieTile key={movie.id} movie={movie} />
-          ))}
-        </Flex>
-      );
+  const filteredMovies = data.filter(movie => (movie.title.toLowerCase().includes(search.toLowerCase()) || movie.release_date.includes(search.toString()) ))
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get('https://ghibliapi.vercel.app/films');
+        setData(result.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Box>
+      <Input display='flex' placeholder='Filter by name or year' size='sm' width='384px' marginY='3' marginX='auto' onChange={handleSearch} />
+      <Box display='flex' flexWrap='wrap' justifyContent={'space-evenly'}>
+        {filteredMovies.map(movie => (
+          <MovieTile key={movie.id} movie={movie} />
+        ))}
+      </Box>
+    </Box>
+  );
 }
 
 export default HomePage
